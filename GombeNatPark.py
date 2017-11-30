3b5b5f4537a4bb03a1dfcb820d04e7e697b43360
vasiapars <vasiliki.chira@gmail.com>
2017-12-01 01:50:50 +0200

    Assignment2

--- GombeNatPark.py
+++ GombeNatPark.py
@@ -17,3 +17,6 @@ print('b)The number of chimpanzees with 3 digits in their code is {}'.format(len
 avg_diff_decs_conv = abs((sumdf['conv']-sumdf['decs']).mean())
 print('c) The average difference between conventional and decisive traits is {:.3f}'.format(avg_diff_decs_conv))
 
+grouped_by_sex = sumdf[sumdf.columns[-6:]].copy()]groupby(['sex'])
+prominence = grouped_by_sex.mean()
+print(prominence)
