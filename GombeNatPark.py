8c4cb4075a88d1eb62e182eb9d7ae01ea517d111
vasiapars <vasiliki.chira@gmail.com>
2017-11-30 02:44:24 +0200

    GombeNatPark

---
+++ GombeNatPark.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+summary_filename = 'data/gombe_128.csv'
+ratings_filename = 'data/gombe_460.csv'
+
+sumdf = pd.read_csv(summary_filename)
+ratingsdf = pd.read_csv(ratings_filename)
+
+impl = sumdf.impl
+print('a)The median impulsiveness score is {:.2f}'.format(impl.median()))
+
+chimcode_dig =sumdf.chimpcode[sumdf.chimpcode.str.contains(r'\d{3}.*')]
+print('b)The number of chimpanzees with 3 digits in their code is {}'.format(len(chimcode_dig)))
+
+avg_diff_decs_conv = abs((sumdf['conv']-sumdf['decs']).mean())
+print('c) The average difference between conventional and decisive traits is {:.3f}'.format(avg_diff_decs_conv))
+
